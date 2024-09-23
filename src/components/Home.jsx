8e2a508201db36c47b5738dbb47f81f737df92df
mrhacker077 <162405562+mrhacker077@users.noreply.github.com>
2024-09-23 22:53:51 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -41,8 +41,7 @@ const Home = () => {
         </div>
         <div className="right">
           <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
-{/*             <img src={`/assets/${hero.imgSrc}`} alt="hero" /> */}
-            <img src={require(`../assets/${hero.imgSrc}`)} alt="hero" />
+            <img src={`/assets/${hero.imgSrc}`} alt="hero" />
 
           </div>
         </div>
